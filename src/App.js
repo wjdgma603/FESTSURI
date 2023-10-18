4f402b06ee6556de500d757a631be60f52b350ec
wjdgma603 <wjdgma603@naver.com>
2023-10-18 18:42:41 +0900

    폰트 테스트

--- src/App.js
+++ src/App.js
@@ -6,10 +6,14 @@ function App() {
   return (
     <div className="App">
       <Header/>
-      <h1>FIRST team project</h1>
-      <h1>한글 폰트 테스트</h1>
-      <h1>123456789</h1>
-      <h1>SSSSSSSSSSSS</h1>
+        <h1>대제목 40rem bold #111 -4%</h1>
+        <h2>중제목 32rem -4%</h2>
+        <h3>소제목 24rem</h3>
+        <h4>강조폰트</h4>
+        <h5>포인트폰트</h5>
+        <p>기본폰트</p>
+
+
     </div>
   );
 }
