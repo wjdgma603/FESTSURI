000e4d3b27af1325ea8bd06cccb902df044902a7
wjdgma603 <wjdgma603@naver.com>
2023-10-31 17:51:50 +0900

    threejs 구현

--- src/components/Pages/Main/Main.js
+++ src/components/Pages/Main/Main.js
@@ -1,31 +1,77 @@
 import './Main.css'
 import * as THREE from 'three'
+import { useEffect } from 'react';
+import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
+import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
+import model from './model/scene.gltf'
 
 
 const Main = () => {
+    useEffect(() => {
+        //GLTF Loader
+        const gltfLoader = new GLTFLoader();
+        let mixer;
+        gltfLoader.load(model, (gltf)=>{    
+            const imgs = gltf.scene.children[0];
+            console.log(imgs)
+            imgs.position.x = -1;
+            imgs.position.y = -1.3;
+            scene.add(imgs)
+            camera.lookAt(imgs)
+            mixer = new THREE.AnimationMixer( gltf.scene);
 
-    const scene = new THREE.Scene();
-    const camera = new THREE.PerspectiveCamera(
-        75,
-        ThreeSec.clientWidth / ThreeSec.clientHeight,
-        0.1,
-        100
-    );
-    const renderer = new THREE.WebGLRenderer({
-        antialias : true
-    });
-    var ThreeSec = document.getElementById("ThreeS")
-    // ThreeSec.appendChild( renderer.domElement )
-    // setTimeout(()=>{ThreeSec.appendChild( renderer.domElement )},1000); 
-    // renderer.setPixelRatio(window.devicePixelRatio > 1 ? 2 : 1) 
-    // renderer.setClearAlpha(0.5) 
-    // renderer.setClearColor('#000') // 배경의 색상 설정
-    // scene.background = new THREE.Color('#000')// 배경 색 설정
-    // renderer.setSize( ThreeSec.clientWidth, ThreeSec.clientHeight );
-    // console.log(ThreeSec)
-    // renderer.render(scene, camera);
+            
+            const actions = [];
+            actions[0] = mixer.clipAction(gltf.animations[0]);
+            actions[1] = mixer.clipAction(gltf.animations[1]);
+            actions[2] = mixer.clipAction(gltf.animations[2]);
+            actions[0].play();
+        })
+        
+        //Renderer Setting
+        var canvReference = document.getElementById("my_canvas");
+        const renderer = new THREE.WebGLRenderer({antialias : true, alpha : true, canvas: canvReference});
+        renderer.setPixelRatio(window.devicePixelRatio > 1 ? 2 : 1) 
+        renderer.setSize(canvReference.clientWidth, canvReference.clientHeight); 
+        
+        //scene
+        const scene = new THREE.Scene();
 
+        //camera
+        const camera = new THREE.PerspectiveCamera(47,canvReference.clientWidth/canvReference.clientHeight,0.1,1000);
+        scene.add(camera)   
+        camera.position.z = 5;
+        camera.position.y = 0;
+        camera.position.x = 2;
 
+        // light 
+        const ambientLight = new THREE.AmbientLight('white', 4);
+        scene.add(ambientLight);
+
+        // 컨트롤 셋팅
+        const controls = new OrbitControls( camera, renderer.domElement );
+        controls.enabled = true
+        const clock = new THREE.Clock();
+        const animate = ()=>{
+                const time = clock.getElapsedTime();
+            controls.update()
+            if(mixer) mixer.update(time)
+            renderer.render(scene, camera)
+            renderer.setAnimationLoop(animate)
+        }
+        animate();
+
+        renderer.render(scene, camera)
+        // 창 크기에 따라 사이즈 조절
+        window.addEventListener('resize', ()=>{
+            camera.aspect = canvReference.innerWidth / canvReference.innerHeight;
+            camera.updateProjectionMatrix();
+            renderer.setSize(canvReference.innerWidth, canvReference.innerHeight);
+            renderer.render(scene, camera);
+        })
+
+    
+    }, [])
     return ( 
         <main className="Main">
             <section className="Main_IntroSection">
@@ -33,7 +79,10 @@ const Main = () => {
                     <h1>아름다운 축제와<br className='Mobile_space'/>행복한 추억이 가득한<br className='Mobile_space'/><span>페스츠리</span>와 함께</h1>
                     <p>패스츠리 지금 여기에서 특별한 여정을, 다채로운 문화, 예술, 그리고 무수한 활동으로 가득 차 있습니다.</p>
                 </article>
-                <div id='ThreeS'></div>
+                <article className='Three_Sector'>
+                    <div className='ThreeBackLight'></div>
+                    <canvas id='my_canvas'></canvas>
+                </article>
             </section>
             <section className="Main_SlideSection">
 
