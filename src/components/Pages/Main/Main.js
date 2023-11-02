5b058429c9b15ba6f445b0a5d4e95b57f6b4cec2
wjdgma603 <wjdgma603@naver.com>
2023-11-02 15:22:17 +0900

    Main.js 정리

--- src/components/Pages/Main/Main.js
+++ src/components/Pages/Main/Main.js
@@ -75,49 +75,44 @@ const Main = () => {
     let threeTimesEvents = []; // 슬라이드 앞 뒤로 복사하는 내용을 저장하는 배열
     const NextEnd = TotalSlides - 2; // 마지막 슬라이드 마지막 부분
     const NextStart = (TotalSlides * 1/3) + 3; // 마지막 슬라이드 시작 부분 
-    const START = (TotalSlides * 2/3) + 1; // 기본 슬라이드 위치
+    const START = (TotalSlides * 2/3) - 1; // 기본 슬라이드 위치
     const PrevStart = 3 // 첫번째 슬라이드 시작부분
     const PrevEnd = (TotalSlides * 2/3) - 2; //첫번째 슬라이드 마지막부분
     const [slideState, setSlideState] = useState({number: START,}) 
 
-    const SlideId = useRef(0); // 슬라이드 id 각자 다르게 생성, 컴포넌트 고유 키값
+    const SlideId = useRef(0);
 
     async function loadEvents(){
         const events = SlideEvent;
         threeTimesEvents = [...events, ...events, ...events];
         setEventsState(threeTimesEvents);
     }
-    // Json내용 복사 후 전개, State문 안에 저장
-
     function setInitialPosition() {
         SlideRef.current.style.transform = `translateX(-${(SlideWidth + SlideMargin) * (MaxSlides - 1)}px)`;
     }
-    //첫번째로 노출할 슬라이드의 값
     function moveTo(setNumber, setMotion){
         setSlideState({
             memo: slideState.number,
             number: setNumber,
             hasMotion: setMotion
         })
-    }
-    //State문에 저장된 memo(슬라이드 번호), number(슬라이드 위치), hasMotion(전환효과 사용유무)
+    }// 슬라이드 움직이는 함수(슬라이드 번호, 전환효과)
+
     function slideAfterMove(setNumber, setMotion) {
         setTimeout(()=>{
             moveTo(setNumber, setMotion)
         }, 1)
-    }
-    //Slide가 움직일 때 0.001초의 지연시간을 줌
+    }// 슬라이드 움직이는 함수 지연시간
+
+
+
     function handleSlideRight() {
         if(slideState.number === NextEnd && slideState.memo === NextEnd - 1) {
             moveTo(PrevEnd, false);
             slideAfterMove(PrevEnd + 1, true);
-            //슬라이드 번호와 NextEnd의 번호일치, 슬라이드 현 위치가 NextEnd의 전 위치
-            //전환효과를 비활성화하고 PrevEnd로 이동, 전환효과를 활성화하고 한칸 앞으로 이동
         } else if (slideState.number === NextStart && slideState.memo === NextStart - 1){
             moveTo(PrevStart, false)
             slideAfterMove(PrevStart, true);
-            //슬라이드 번호와 NextStart의 번호일치, 슬라이드 현 위치가 NextStart의 전 위치
-            //전환효과를 비활성화하고 PrevEnd로 이동, 전환효과를 활성화하고 한칸 앞으로 이동
         } else {
             moveTo(slideState.number + 1, true);
         }
@@ -133,16 +128,16 @@ const Main = () => {
             moveTo(slideState.number - 1, true);
         }
     }
+    // 슬라이드 버튼 함수
     useEffect(()=>{
         loadEvents()
         setInitialPosition();
+        // eslint-disable-next-line react-hooks/exhaustive-deps
     },[])
-    
     useEffect(()=>{
         SlideRef.current.style.transform = `translateX(-${slideState.number * (SlideWidth + SlideMargin)}px)`
         SlideRef.current.style.transition = slideState.hasMotion ? 'all 0.3s ease-in-out' : '';
     }, [slideState])
-    console.log(slideState.number)
     return ( 
         <main className="Main">
             <section className="Main_IntroSection">
