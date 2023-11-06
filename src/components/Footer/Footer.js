f64435d5de9ae37d5cbb23d445025428ab30bd63
wjdgma603 <wjdgma603@naver.com>
2023-11-06 17:54:49 +0900

    footer 수정

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -3,7 +3,7 @@ const Footer = () => {
         <footer className="Footer">
             <section className="Footer_Wrap">
                 <article className="Footer_FirstWrap">
-                    <div className="Footer_Logo"></div>
+                    <div className="Footer_Logo">ss</div>
                 </article>
                 <article className="Footer_SecondWrap"></article>
                 <article className="Footer_Wrap"></article>
