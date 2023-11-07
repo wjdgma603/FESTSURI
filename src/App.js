b8be8c21e1ffc33898f0ed91f4b2f5ee5d0cb0ef
wjdgma603 <wjdgma603@naver.com>
2023-11-07 17:49:55 +0900

    로그인 페이지 구현 30%

--- src/App.js
+++ src/App.js
@@ -31,10 +31,10 @@ function App() {
     useEffect(() => {
       setIsFooterLoaded(true);
       return () => {
-        setIsHeaderLoaded(false);
+        setIsFooterLoaded(false);
       };
     }, []);
-    return isHeaderLoaded;
+    return isFooterLoaded;
   }
   // 컴포넌트 마운트 시 True, 언마운트시 False 반환.
   return (
@@ -42,7 +42,7 @@ function App() {
       <Header Loaded={isHeaderLoaded}/>
         <Routes>
           <Route path='/' element={<Main IsHeaderLoaded={IsHeaderLoaded}/>}/>
-          <Route path='/login' element={<Login/>}/>
+          <Route path='/login' element={<Login IsFooterLoaded={IsFooterLoaded}/>}/>
           <Route path='/party' element={<Party/>}/>
           <Route path='/exhibition' element={<Exhibition/>}/>
           <Route path='/exbition/detail' element={<PtyExbDetail/>}/>
@@ -50,7 +50,7 @@ function App() {
           <Route path='/gallery' element={<Gallery/>}/>
           <Route path='/introduce' element={<Introduce/>}/>
         </Routes>
-      <Footer Loaded={isFooterLoaded}/>
+      <Footer FtLoaded={isFooterLoaded}/>
     </div>
   );
 }
--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -3,11 +3,16 @@ import NormalFooter from "./Components/NormalFooter";
 import SimpleFooter from "./Components/SimpleFooter";
 
 
-const Footer = (Loaded) => {
-
-    return ( 
-        <NormalFooter/>
-     );
+const Footer = ({FtLoaded}) => {
+    return(
+        <div>
+            {FtLoaded ?
+            <SimpleFooter/>
+            :
+            <NormalFooter/>
+            }
+        </div>
+    );
 }
  
 export default Footer;
