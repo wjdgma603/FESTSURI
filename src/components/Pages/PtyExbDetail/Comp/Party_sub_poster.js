346e05b8047780b57f842866528925363b050f9c
the100year <the100year@gmail.com>
2023-11-07 10:32:16 +0900

    카카오 맵 구현

--- src/components/Pages/PtyExbDetail/Comp/Party_sub_poster.js
+++ src/components/Pages/PtyExbDetail/Comp/Party_sub_poster.js
@@ -1,8 +1,9 @@
 
 import './Party_sub_poster.css'
+import dlsk from  '../'
 
 const Party_sub_poster = ({ id, title, sub_title1, sub_title2, sub_title3, sub_title4, sub_title5, sub_title6, sub_title7, sub_title8,
-    party_info1, party_info2, party_info3, party_info4, party_info5, party_info6, party_info7 }) => {
+    party_info1, party_info2, party_info3, party_info4, party_info5, party_info6, party_info7,sub_img }) => {
     return (
         <div className='Party_sub_poster'>
 
@@ -24,6 +25,7 @@ const Party_sub_poster = ({ id, title, sub_title1, sub_title2, sub_title3, sub_t
                 <p className='Party_sub_poster_party_info5'>{party_info5}</p>
                 <p className='Party_sub_poster_party_info6'>{party_info6}</p>
                 <p className='Party_sub_poster_party_info7'>{party_info7}</p>
+                <img className='Party_sub_map' src={require("../images/map" + id + ".png")} />
             </div>
 
         </div>
