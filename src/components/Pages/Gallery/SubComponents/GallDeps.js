a3a551a4cda08234de5bc951a783c99bdde814d0
wjdgma603 <wjdgma603@naver.com>
2023-11-09 17:40:31 +0900

    CSS 수정

--- src/components/Pages/Gallery/SubComponents/GallDeps.js
+++ src/components/Pages/Gallery/SubComponents/GallDeps.js
@@ -6,13 +6,14 @@ import { Link } from "react-router-dom";
 const GallDeps = () => {
   return (
     <section className="GallDeps">
-      <article className="Gall_DepsPadding"> 
+      <article className="Gall_DepsWrap"> 
         <div className="Gall_BtnFlex">
           <div className="Gall_DepsBtn">
             <Link to="/">홈</Link></div>
-          <div className="Gall_Center">＞</div>
+          <div className="Gall_Center"></div>
           <div className="Gall_DepsBtn">
-            <a>참여갤러리</a></div>
+            <Link to="/gallery">참여갤러리</Link>
+          </div>
         </div>
       </article>
     </section>
