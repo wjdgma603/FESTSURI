a451e3d23142d00c127a4db371258651cf0911ca
wjdgma603 <wjdgma603@naver.com>
2023-11-09 11:24:32 +0900

    파티서브

--- src/components/Pages/PtyExbDetail/party_sub.js
+++ src/components/Pages/PtyExbDetail/party_sub.js
@@ -1,6 +1,6 @@
 import './Party_sub.css'
 import Party_sub_comp from './Comp/Party_sub_poster.json'
-import { useLocation } from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom';
 import { useState } from 'react';
 import { MapMarker, Map } from 'react-kakao-maps-sdk'
 function Party_sub() {
@@ -12,29 +12,25 @@ function Party_sub() {
     const [modal, setmodal] = useState(false);
     const Party_sub_modal1 = () => {
         setmodal(true)
-
     }
     const Party_sub_modal2 = () => {
         setmodal(false)
-
     }
 
-
     return (
-
         <div className="Party_sub">
             <section className='Party_sub_title'>
                 <ul className='party_go'>
-                    <li><a>홈</a></li>
+                    <li><Link to='/'>홈</Link></li>
                     <li className='party_arrow'></li>
-                    <li><a>행사</a></li>
-                </ul>
+                    <li><Link to='/party'>행사</Link></li>
+                </ul>   
             </section>
             {party_list.map((list) => (
                 <div key={Party_sub}>
                     <section className='Party_sub_hitop'>
                         <div className='Party_sub_top'>
-                            <img src={require('./images/psp' + list.id + '.png')} />
+                            <img src={require('./images/psp' + list.id + '.png')} alt='PartySubImages' />
                             <ul className='Party_sub_po'>
                                 <li className='Party_sub_title_name'>{list.title}</li>
                                 <li className='Party_sub_line'></li>
